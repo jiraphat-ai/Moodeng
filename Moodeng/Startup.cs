5a6fdcd41eb8b95abc1d421a1c7105d3826e610d
ballalone1234 <ballpol.127@gmail.com>
2024-11-25 15:10:34 +0700

    Add project files.

---
+++ Moodeng/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Moodeng.Startup))]
+namespace Moodeng
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
