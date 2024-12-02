da1b23c5c3a46811edc9a819cc7ccbe9b8e1ac05
S_ZOOM <pawatsaekoo@gmail.com>
2024-12-02 14:28:19 +0700

    frontend (login regsiter  หน้า home นิดหน่อย)

---
+++ Moodeng/Scripts/main.js
@@ -0,0 +1,11 @@
+﻿
+// navbar toggle
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.toggle('max-[768px]:h-[280px]');
+        navMenu.classList.toggle('max-[768px]:h-0');
+    });
+}
